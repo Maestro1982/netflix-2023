9f8d920a16501ecc37ded8b3f4bfc87dd7696efa
Maestro1982 <kris.tote@telenet.be>
2023-02-10 13:25:13 +0100

    create tmdb.configs.js

---
+++ client/src/api/configs/tmdb.configs.js
@@ -0,0 +1,27 @@
+const mediaType = {
+  movie: 'movie',
+  tv: 'tv',
+};
+
+const mediaCategory = {
+  popular: 'popular',
+  top_rated: 'top_rated',
+};
+
+const backdropPath = (imageEndpoint) =>
+  `https://image.tmdb.org/t/p/original${imageEndpoint}`;
+
+const posterPath = (imageEndpoint) =>
+  `https://image.tmdb.org/t/p/w500${imageEndpoint}`;
+
+const youtubePath = (videoId) => `https://www.youtube.com/embed/${videoId}`;
+
+const tmdbConfigs = {
+  mediaType,
+  mediaCategory,
+  backdropPath,
+  posterPath,
+  youtubePath,
+};
+
+export default tmdbConfigs;
