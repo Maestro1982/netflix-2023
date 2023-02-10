546cbb0aeb0dcf3346b1020f308324738b67dbd4
Maestro1982 <kris.tote@telenet.be>
2023-02-10 15:35:44 +0100

    create themeModeSlice, modify store.js and main.jsx

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../redux/features/userSlice.js';
+import themeModeSlice from './features/themeModeSlice.js';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    themeMode: themeModeSlice,
   },
 });
 
